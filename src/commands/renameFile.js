a4e9f619445e4853d3333e9b0c938a3ccc374733
Ol_Kuts <kuts.ol.vas@gmail.com>
2022-12-10 19:28:27 +0200

    refactor: rename file

--- src/commands/renameFile.js
+++ src/commands/renameFile.js
@@ -1,9 +1,11 @@
 import {rename} from 'node:fs/promises'
+import {resolve, dirname} from 'path'
 import {getColorText, outFlag, outCurrentlyDir} from '../utils/index.js'
 
 export const renameFile = async (oldFileName, newFileName) => {
+  const dir = dirname(resolve(oldFileName))
   try {
-    await rename(oldFileName, newFileName)
+    await rename(oldFileName, resolve(dir, newFileName))
     outCurrentlyDir()
   } catch (e) {
     console.log(getColorText(e.message, outFlag.ERROR))
